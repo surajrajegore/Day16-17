b1912cf26bcfcaaf25bffb8d055c7f9f522c16a4
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-17 12:29:35 +0530

    Added:code of generate string permutation usng iterative and recursive approach

--- src/com/blz/stringpermutations/StringPermutation.java
+++ src/com/blz/stringpermutations/StringPermutation.java
@@ -1,10 +1,59 @@
 package com.blz.stringpermutations;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringPermutation {
     public static void main(String[] args){
-        System.out.println("welcome to permutation of string program");
 
+        String str = "ABC";
+        System.out.println("Using Recursive Permutation");
+        recursivePermutations("", str);
+        System.out.println("Using Itarative Approach");
+        iterativePermutation(str);
+    }
+    private static void recursivePermutations(String candidate, String remaining)
+    {
+        if (remaining == null) {
+            return;
+        }
+
+        if (remaining.length() == 0) {
+            System.out.println(candidate);
+        }
+
+        for (int i = 0; i < remaining.length(); i++)
+        {
+            String newCandidate = candidate + remaining.charAt(i);
+
+            String newRemaining = remaining.substring(0, i) +
+                    remaining.substring(i + 1);
+
+            recursivePermutations(newCandidate, newRemaining);
+        }
+    }
+    public static void iterativePermutation(String str)
+    {
+        if (str == null || str.length() == 0) {
+            return;
+        }
+        List<String> partial = new ArrayList<>();
+        partial.add(String.valueOf(str.charAt(0)));
+
+        // do for every character of the specified string
+        for (int i = 1; i < str.length(); i++)
+        {
+            for (int j = partial.size() - 1; j >= 0 ; j--)
+            {
+                String s = partial.remove(j);
+
+                for (int k = 0; k <= s.length(); k++)
+                {
+                    partial.add(s.substring(0, k) + str.charAt(i) + s.substring(k));
+                }
+            }
+        }
+
+        System.out.println(partial);
     }
 }
