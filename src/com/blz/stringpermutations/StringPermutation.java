734862c2f68a31f83919dc0b5fdddff88f0f9001
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-17 10:25:50 +0530

    Added:welcome message on  master

---
+++ src/com/blz/stringpermutations/StringPermutation.java
@@ -0,0 +1,10 @@
+package com.blz.stringpermutations;
+
+import java.util.Scanner;
+
+public class StringPermutation {
+    public static void main(String[] args){
+        System.out.println("welcome to permutation of string program");
+
+    }
+}
